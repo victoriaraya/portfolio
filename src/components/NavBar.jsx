0c8cfd9823e96158bfa7863093aa764fc74f16be
Victoria Raya <victoriaraya111@gmail.com>
2024-08-14 14:52:31 -0700

    removed comments from navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// const NavBar = () => {
-//   return (
-//     <ul className="flex flex-row gap-8 justify-center text-center p-3 text-2xl text-white font-bold border-b-2 border-white">
-//       <li className="hover:[text-shadow:1px_1px_3px_white]">
-//         <Link to="/">Home</Link>
-//       </li>
-//       <li className="hover:[text-shadow:1px_1px_3px_white]">
-//         <Link to="/expertise">Expertise</Link>
-//       </li>
-//       <li className="hover:[text-shadow:1px_1px_3px_white]">
-//         <Link to="/projects">Projects</Link>
-//       </li>
-//       <li className="hover:[text-shadow:1px_1px_3px_white]">
-//         <Link to="/about-me">About Me</Link>
-//       </li>
-//       <li className="hover:[text-shadow:1px_1px_3px_white]">
-//         <Link to="/contact">Contact</Link>
-//       </li>
-//     </ul>
-//   );
-// };
-
-// export default NavBar;
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
