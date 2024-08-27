59fefd8674bf5c755ec3dd909694a2442c19c497
Victoria Raya <victoriaraya111@gmail.com>
2024-08-27 16:00:54 -0700

    styling for project details page

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams, Navigate, Link } from "react-router-dom";
 import { getProject } from "../projects";
+import { GoArrowUpRight } from "react-icons/go";
 
 const ProjectDetails = () => {
   const { name } = useParams();
@@ -10,39 +11,56 @@ const ProjectDetails = () => {
   }
 
   return (
-    <div className="h-screen w-screen mt-0 sm:pt-9 bg-[#b291a4] text-white flex flex-col">
-      <p>{project.name}</p>
-      <p>{project.description}</p>
-      <div className="flex flex-row gap-2">
-        {project.technologies.map((tech) => (
-          <p>{`${tech}`}</p>
-        ))}
-      </div>
-      <a href={`${project.url}`} target="_blank" rel="noopener noreferrer">
-        {project.url.includes("github") ? "See code" : "See website"}
-      </a>
-      <div>
-        {project.images[0].includes("jpg") ? (
-          <img
-            src={`/images/${project.images[0]}`}
-            alt={`${project.name} page`}
-            className="h-60 w-1/3"
-          />
-        ) : (
-          project.images.map((image, idx) =>
-            idx == 0 ? null : (
-              <img
-                src={`/images/${image}`}
-                alt={`${project.name} page`}
-                className="h-60 w-1/3"
-              />
-            ),
-          )
-        )}
-      </div>
-      <p>
-        <Link to="/projects">Go back</Link>
+    <div className="min-h-screen w-screen mt-0 sm:pt-9 px-6 bg-[#b291a4] text-white text-2xl flex flex-col">
+      <p className="flex justify-end z-20 -mt-3">
+        <Link to="/projects" className="hover:[text-shadow:1px_1px_3px_white]">
+          Go back
+        </Link>
       </p>
+      <div className="grid grid-cols-2 grid-rows-3">
+        <h1 className="col-span-2 text-8xl -mt-6">{project.name}</h1>
+        <p className="justify-left row-start-2 -mt-4">
+          <a
+            href={`${project.url}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative inline-flex items-center gap-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-300 hover:after:w-full"
+          >
+            <GoArrowUpRight className="text-xl" />
+            {project.url.includes("github") ? "See code" : "See website"}
+          </a>
+        </p>
+        <div className="row-start-3 flex flex-col -mt-20 mr-5">
+          <span className="pb-2">Tools used:</span>
+          <span className="flex flex-wrap gap-3">
+            {project.technologies.map((tech) => (
+              <span
+                className="bg-slate-300 bg-opacity-35 rounded-md p-1"
+                key={tech}
+              >
+                {tech}
+              </span>
+            ))}
+          </span>
+        </div>
+        <p className="row-span-2 text-left px-2 -mt-10">
+          {project.description}
+        </p>
+      </div>
+      <div className="flex flex-wrap gap-4 pt-8 pb-3 px-5">
+        {project.images[0].includes("jpg")
+          ? null
+          : project.images.map((image, idx) =>
+              idx == 0 ? null : (
+                <img
+                  src={`/images/${image}`}
+                  alt={`${project.name} page`}
+                  key={idx}
+                  className="rounded-sm border-2 border-white"
+                />
+              ),
+            )}
+      </div>
     </div>
   );
 };
