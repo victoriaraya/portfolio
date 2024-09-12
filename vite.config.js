6664302ced73720230d21c36766b42387c645525
Victoria Raya <victoriaraya111@gmail.com>
2024-09-12 15:04:05 -0700

    update to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   plugins: [react()],
   root: "src",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+      },
+    },
   },
 });
